343b881c662572e7f45e74f079e046b22d5cc067
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-01-31 22:58:49 +0100

    :sparkles: add /test endpoint

--- lib/index.ts
+++ lib/index.ts
@@ -6,6 +6,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.WEBSERVER_PORT || 8000;
 
+app.get("/test", async (req, res) => {
+  return res.send({ id: 123456789, test: "test", test2: "test2" });
+});
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}/`);
 });
