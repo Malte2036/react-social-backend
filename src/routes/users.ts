a0ffa786013e43fcb301c4f7cf2b843ae9705d1d
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-02-01 00:17:07 +0100

    :sparkles: add find user by id endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,7 +7,7 @@ export function usersController(app: Express, connection: Connection) {
    * @swagger
    * /users:
    *   get:
-   *     description: Returns all users
+   *     summary: Get all users
    *     tags:
    *      - Users
    *     produces:
@@ -19,5 +19,40 @@ export function usersController(app: Express, connection: Connection) {
   app.get("/users", async (req, res) => {
     const users = await connection.manager.find(User);
     return res.json(users);
+  });
+
+  /**
+   * @swagger
+   * /user/{userId}:
+   *   get:
+   *     summary: Find user by id
+   *     tags:
+   *      - Users
+   *     produces:
+   *      - application/json
+   *     parameters:
+   *        - name: userId
+   *          in: path
+   *          description: ID of user to return
+   *          required: true
+   *          schema:
+   *            type: integer
+   *            format: int64
+   *     responses:
+   *       200:
+   *         description: user
+   */
+  app.get("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    if (userId === undefined) {
+      return res.status(400).end();
+    }
+    console.log(userId);
+
+    const users = await connection.manager.findByIds(User, [userId]);
+    if (users.length == 0) {
+      return res.status(404).end();
+    }
+    return res.json(users[0]);
   });
 }
