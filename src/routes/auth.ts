902394f19fc76f0ed3883fef8a3b72eaffa3b740
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-02-01 16:25:10 +0100

    :sparkles: hash passwords

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Connection } from "typeorm";
 import { Express } from "express";
 import { generateAccessToken } from "../services/authService";
-import { User } from "../entity/User";
+import { findUserByEmail, registerUser } from "../services/userService";
+import { compareHash } from "../services/hash";
 
 export function authController(app: Express, connection: Connection) {
   /**
@@ -37,10 +38,25 @@ export function authController(app: Express, connection: Connection) {
    *       200:
    *         description: token
    */
-  app.post("/auth/register", (req, res) => {
-    const token = generateAccessToken(req.body.email as string);
+  app.post("/auth/register", async (req, res) => {
+    if (
+      req.body.email === undefined ||
+      req.body.name === undefined ||
+      req.body.password === undefined
+    ) {
+      return res.sendStatus(500);
+    }
+    console.log("register");
+    const user = await registerUser(
+      connection,
+      req.body.email,
+      req.body.name,
+      req.body.password
+    );
 
-    res.json({ token: token });
+    const token = generateAccessToken(user.email as string);
+
+    res.json({ token: token, user: user });
   });
 
   /**
@@ -67,14 +83,30 @@ export function authController(app: Express, connection: Connection) {
    *             password:
    *               type: string
    *     responses:
-   *       405:
-   *         description: Invalid input
-   *         content: {}
    *       200:
-   *         description: token
+   *         description: 
+   *       403:
+   *         description: 
+   *       404:
+   *         description: 
+   *       500:
+   *         description: 
    */
-  app.post("/auth/login", (req, res) => {
+  app.post("/auth/login", async (req, res) => {
+    if (req.body.email === undefined || req.body.password === undefined) {
+      return res.sendStatus(500);
+    }
+
+    const user = await findUserByEmail(connection, req.body.email);
+    if (user === undefined) {
+      return res.sendStatus(404);
+    }
+    const valid = await compareHash(user.password, req.body.password);
+    if (!valid) {
+      return res.sendStatus(403);
+    }
+
     const token = generateAccessToken(req.body.email as string);
-    res.json({ token: token });
+    res.status(200).json({ token: token, user: user });
   });
 }
---
+++ src/services/userService.ts
@@ -0,0 +1,28 @@
+import { Connection } from "typeorm";
+import { User } from "../entity/User";
+import { hashPlainText } from "./hash";
+
+export async function registerUser(
+  connection: Connection,
+  email: string,
+  name: string,
+  password: string
+) {
+  const passwordHash = await hashPlainText(password);
+
+  const user = new User();
+  user.name = name;
+  user.email = email;
+  user.password = passwordHash;
+
+  return await connection.manager.save<User>(user);
+}
+
+export async function findUserByEmail(
+  connection: Connection,
+  email: string
+): Promise<User | undefined> {
+  return await connection.getRepository(User).findOne({
+    where: { email: email },
+  });
+}
