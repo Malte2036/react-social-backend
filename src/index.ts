4826affde5f39e89fa54225998c3289775b344b4
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-02-01 14:25:59 +0100

    :fire: remove user creation on start

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { User } from "./entity/User";
 import routes from "./routes/routes";
 
 createConnection()
   .then(async (connection) => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.name = "User";
-    user.email = "user@user.de";
-    user.password = "12j32n2"
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
     routes(connection);
   })
   .catch((error) => console.log(error));
