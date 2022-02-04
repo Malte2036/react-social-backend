2f32c86301411d3d7c36c4a79dc381ec0e0e81df
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-02-04 13:22:04 +0100

    :fire: delete all

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import routes from "./routes/routes";
-
-createConnection()
-  .then(async (connection) => {
-    routes(connection);
-  })
-  .catch((error) => console.log(error));
