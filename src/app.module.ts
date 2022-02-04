28dd8171201b371bfea08338cdbbb5b09b254d93
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-02-04 13:49:29 +0100

    :heavy_plus_sign: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'react-social',
+      password: 'react-social',
+      database: 'react-social',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
