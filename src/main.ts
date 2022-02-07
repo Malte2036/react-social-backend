572680c0fcaf6204c9cff25e499fe2fd927d88b2
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-02-07 15:41:07 +0100

    :wrench: enable cors

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
